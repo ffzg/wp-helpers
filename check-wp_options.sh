b91b96a5a91d02de8b2f4f54a386422eecbf137d
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-06-21 10:16:46 +0200

    check all wp-config.php under /srv/www

--- check-wp_options.sh
+++ check-wp_options.sh
@@ -4,12 +4,9 @@ name=$( basename $0 | sed -e 's/^check-//' -e 's/\.sh$//' )
 
 echo "# $name" > /dev/stderr
 
-ls -d /srv/www/* | while read dir ; do
+find /srv/www/ -name wp-config.php | while read config ; do
+	dir=$( dirname $config )
 	cd $dir
-	if [ -e wp-config.php ] ; then
-		value=$( wp db query "select option_value from wp_options where option_name='$name'" | grep -v option_value )
-		echo "$dir $value"
-	else
-		echo "$dir MISSING wp-config.php" > /dev/stderr
-	fi
+	value=$( wp db query "select option_value from wp_options where option_name='$name'" | grep -v option_value )
+	echo "$dir $value"
 done
