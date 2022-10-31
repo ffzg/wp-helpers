3ba85bdc288f8efeb4d617ddd9703b4cb5473f10
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-10-31 19:38:57 +0100

    limit to today

--- wf-blocked.sh
+++ wf-blocked.sh
@@ -1,6 +1,9 @@
 #!/bin/sh -xe
 
-test ! -z "$1" && cd $1 && echo "# $1" || exit 1
+test ! -z "$1" && cd $1 && echo "# $1"
 
-wp db query 'select inet6_ntoa(ip),hex(ip),countryCode,blockCount,unixday,blockType from wp_wfBlockedIPLog' ||
-wp db query 'select inet6_ntoa(ip),hex(ip),countryCode,blockCount,unixday,blockType from wp_wfblockediplog'
+where=" where unixday = FLOOR(UNIX_TIMESTAMP() / 86400)" # today
+#where=" where unixday > FLOOR(UNIX_TIMESTAMP() / 86400) - 7" # week
+
+wp db query "select inet6_ntoa(ip),countryCode,blockCount,unixday,blockType from wp_wfBlockedIPLog $where" ||
+wp db query "select inet6_ntoa(ip),countryCode,blockCount,unixday,blockType from wp_wfblockediplog $where"
