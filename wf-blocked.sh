667c110ce51cd763bab9dee45e125aeb1375bd51
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-10-31 18:11:13 +0100

    both mixed case and lowercase wp_wfblockediplog

--- wf-blocked.sh
+++ wf-blocked.sh
@@ -2,4 +2,5 @@
 
 test ! -z "$1" && cd $1 && echo "# $1" || exit 1
 
-wp db query 'select inet6_ntoa(ip),hex(ip),countryCode,blockCount,unixday,blockType from wp_wfBlockedIPLog'
+wp db query 'select inet6_ntoa(ip),hex(ip),countryCode,blockCount,unixday,blockType from wp_wfBlockedIPLog' ||
+wp db query 'select inet6_ntoa(ip),hex(ip),countryCode,blockCount,unixday,blockType from wp_wfblockediplog'
