0b2cc7f34d8d329a46649fd358a00546858ec53a
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-04-17 11:19:00 +0200

    use wp option get

--- check-wp_options.sh
+++ check-wp_options.sh
@@ -10,6 +10,5 @@ echo "# $name" > /dev/stderr
 find /srv/www/ -name wp-config.php | while read config ; do
 	dir=$( dirname $config )
 	cd $dir
-	value=$( wp db query "select option_value from wp_options where option_name='$name'" | grep -v option_value )
-	echo "$dir $value"
+	echo "$dir " $( wp option get $name )
 done
