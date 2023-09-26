cbf3b1113e1826ca4522af9703fe2887eff5ec0b
Dobrica Pavlinusic <dpavlin@rot13.org>
2023-09-26 09:56:54 +0200

    remove backup user administrator role and change password

---
+++ wp-all-remove-backup-user.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "# $name" > /dev/stderr
+
+login=wordpress
+login=backup
+
+find /srv/www/ -name wp-config.php | while read config ; do
+	dir=$( dirname $config )
+	user=$( ls -al $config | awk '{ print $3 }' )
+	cd $dir
+	echo "# $dir"
+	if sudo -u $user wp user list | grep $login ; then
+		echo "XXX remove"
+		sudo -u $user wp user remove-role $login administrator
+		sudo -u $user wp user update $login --user_pass=$( pwgen 12 1 )
+	fi
+done
