112a85c992172fd6744a39597941245c4b54faaf
Dobrica Pavlinusic <dpavlin@rot13.org>
2025-05-09 10:20:04 +0200

    better output to terminal

--- wp-download-zips.sh
+++ wp-download-zips.sh
@@ -27,7 +27,7 @@ wp_core_version=$(wp core version)
 wp_core_zip="wordpress-${wp_core_version}.zip"
 if [ ! -e "$download_dir/$wp_core_zip" ] ; then
 	core_url="wordpress.org/wordpress-${wp_core_version}.zip"
-	echo "Downloading WordPress Core ${wp_core_version}..."
+	echo "Downloading WordPress Core ${wp_core_version}"
 	wget -q -m "https://$core_url" -P "$download_dir" && echo "Core downloaded successfully" || echo "Failed to download core"
 else
 	echo "Existing $wp_core_zip"
@@ -40,19 +40,21 @@ wp plugin list --fields=name,version --format=csv | tail -n +2 | while IFS=, rea
     plugin_zip="${slug}.${version}.zip"
     plugin_url="downloads.wordpress.org/plugin/${slug}.${version}.zip"
     plugin_url_no_ver="downloads.wordpress.org/plugin/${slug}.zip"
-    echo "Checking ${slug} version ${version}..."
+    echo "Downloding plugin ${slug} ${version}"
 
     if ! wget -q -m "https://$plugin_url" -P "$download_dir" ; then 
 	if ! wget -q -m "https://$plugin_url_no_ver" -P "$download_dir" ; then
 		local_zip=/home/dpavlin/${slug}.${version}.zip
 		if [ -e $local_zip ] ; then
-			unzip -o -q $local_zip    -d /tmp/wp/wordpress/wp-content/plugins/
+			unzip -o -q $local_zip    -d "$wp_dir/wordpress/wp-content/plugins/"
 		else
 			echo "ERROR: ${slug}.${version}.zip MISSING"
 		fi
+        else
+		unzip -o -q $download_dir/$plugin_url_no_ver -d "$wp_dir/wordpress/wp-content/plugins/"
 	fi
     else
-  	unzip -o -q $download_dir/$plugin_url -d /tmp/wp/wordpress/wp-content/plugins/
+  	unzip -o -q $download_dir/$plugin_url -d "$wp_dir/wordpress/wp-content/plugins/"
     fi
 done
 
@@ -60,17 +62,16 @@ done
 echo "Processing themes..."
 wp theme list --fields=name,version --format=csv | tail -n +2 | while IFS=, read -r slug version; do
     theme_url="downloads.wordpress.org/theme/${slug}.${version}.zip"
-    echo "Checking ${slug} version ${version}..."
+    echo "Downloding theme ${slug} ${version}"
     
     if ! wget -q -m "$theme_url" -P "$download_dir" ; then
 	echo "ERROR: $theme_url MISSING"
     else
-    	unzip -o -q $download_dir/$theme_url -d /tmp/wp/wordpress/wp-content/themes/
+    	unzip -o -q $download_dir/$theme_url -d "$wp_dir/wordpress/wp-content/themes/"
     fi
 done
 
 echo "Download process completed!"
-echo "Files saved in:"
-echo "- WordPress core: ${core_dir}"
-echo "- Plugins: ${plugins_dir}"
-echo "- Themes: ${themes_dir}"
+echo "Download dir: $download_dir"
+echo "Wordpress dir: $wp_dir/wordpress/"
+echo "Compare installation with: diff -urw $wp_dir/wordpress $(pwd)"
